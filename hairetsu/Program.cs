9b59f10e04158c2b8972a2ad29fbc155faa05e23
shuikouzhizi7@gmail.com <shuikouzhizi7@gmail.com>
2022-06-16 20:40:26 +0900

    回答

--- hairetsu/Program.cs
+++ hairetsu/Program.cs
@@ -12,17 +12,31 @@ namespace hairetsu
         {
             int[] score = { 87, 41, 34, 64, 10, 52, 19, 71, 95, 26 };
             int[] rank = new int[10];
-            int conun = 0;
-            foreach (int n in score)
+            
+            Console.WriteLine("得点\t 順位");
+
+            for(int i=0; i<score.Length; i++)//score.lengthでscoreの中の要素数
             {
-                // 画面に出力
-                Console.WriteLine("{0}", n);
+                rank[i] = 1;//順位は1から
+                for(int j=0; j<score.Length; j++)//scoreの中の要素をランク１から順に比較していく
+                {
+                    if (score[i] < score[j])
+                    {
+                        rank[i]++;
+                    }
+                }
+                Console.WriteLine(score[i] + "\t" + rank[i]);
+            }
+            ////foreach (int n in score)
+            ////{
+            ////    // 画面に出力
+            ////    Console.WriteLine("{0}", n);
                 
                 //for (int i=0;i<score.Length; i++)
                 //{
 
                 //}
-            }
+            //}
 
         }
     }
